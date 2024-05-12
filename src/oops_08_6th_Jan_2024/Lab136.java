985416fa03c1c02d25d2cec6718132248de1c609
binit4444 <binit4444@gmail.com>
2024-05-11 20:29:28 -0700

    added Lab137

---
+++ src/oops_08_6th_Jan_2024/Lab136.java
@@ -0,0 +1,19 @@
+package src.oops_08_6th_Jan_2024;
+
+public class Lab136 {
+    public static void main(String[] args) {
+
+
+        // If someting is thread safe, we avoid
+        // StringBuffer vs StringBuilder
+        StringBuilder stringBuilder = new StringBuilder("Binit");
+        stringBuilder.reverse();
+        System.out.println(stringBuilder);
+
+
+
+
+
+
+    }
+}
