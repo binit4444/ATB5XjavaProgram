b28e8654d831b3ceddee95fdfa1c17ed18c7c8f4
binit4444 <binit4444@gmail.com>
2024-05-11 21:11:20 -0700

    added Lab137

---
+++ src/oops_08_6th_Jan_2024/Lab137.java
@@ -0,0 +1,37 @@
+package src.oops_08_6th_Jan_2024;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Lab137 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String, I will check for Palindrome");
+        String user_input = scanner.next();
+      //  user_input = user_input.toLowerCase(); // Optional, if using .equalsIgnorecase function
+        boolean result = isPalindrome(user_input);
+        if (result){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not a Palindrome");
+        }
+
+
+    }
+
+    private static boolean isPalindrome(String userInput){
+        String original_str = userInput; // Binit, naman
+        StringBuilder sb = new StringBuilder(userInput);
+        sb.reverse();
+       return original_str.equalsIgnoreCase(sb.toString());
+
+       // Second way
+        //  String reverse_string = sb.reverse().toString(); // tiniB, naman
+        //  return original_str.equalsIgnoreCase(reverse_string); // Binit == tiniB false
+
+    }
+
+
+
+}
